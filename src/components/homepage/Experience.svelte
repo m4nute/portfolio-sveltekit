c6fdc78c04827fbd10eeaf4657125092dfe45d39
m4nute <mgudinocontacto@gmail.com>
2023-08-27 12:02:04 -0300

    Experience section and cards

---
+++ src/components/homepage/Experience.svelte
@@ -0,0 +1,25 @@
+<script>
+  import ExperienceCard from "./ExperienceCard.svelte"
+</script>
+
+<h5 class="underline mb-3">Experience</h5>
+<ol>
+  <li class="flex flex-col gap-y-4">
+    <ExperienceCard
+      project="JustProats"
+      role="Software Developer"
+      description="Built a fully customised Shopify Store from scratch, as the new site for the business. Will be available soon :)"
+      link="https://justproats.com"
+      badges={["Shopify", "Tailwind CSS", "TypeScript", "React.js"]}
+      duration="Jul 2023 - Present"
+    />
+    <ExperienceCard
+      project="Talking Cart"
+      role="Software Developer"
+      description={`Built key functionalities & UX of the chatbot. Engineered the backend to handle & optimize the "brain" logic.`}
+      link="https://talkingcart.com"
+      badges={["Next.js", "TypeScript", "OpenAI API", "Problem Solving"]}
+      duration="Jun - Jul 2023"
+    />
+  </li>
+</ol>
---
+++ src/components/homepage/ExperienceCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { Badge } from "$lib/shadcn/ui/badge"
+  export let badges: string[]
+  export let project: string
+  export let role: string
+  export let link: string
+  export let description: string
+  export let duration: string
+</script>
+
+<article class="flex gap-24">
+  <div class="text-gray-800 dark:text-gray-200 text-sm pt-4 w-32">{duration}</div>
+  <a target="_blank" href={link} class="flex-1 hover:bg-[#ddd] dark:hover:bg-[#112] cursor-pointer p-4 rounded-lg transition-all duration-150">
+    <h6 class="font-bold">{project}</h6>
+    <span class="text-sm">{role}</span>
+    <p class="text-xs text-gray-700 dark:text-gray-300 mb-3">{description}</p>
+    <ol class="gap-2 flex">
+      {#each badges as badge}
+        <Badge>{badge}</Badge>
+      {/each}
+    </ol>
+  </a>
+</article>
