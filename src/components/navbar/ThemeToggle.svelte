63fedbc97bc0bbc726b9e8b7dd02b1a16f0f32bb
m4nute <mgudinocontacto@gmail.com>
2023-08-27 13:40:49 -0300

    Theme Toggle button styled for light theme

--- src/components/navbar/ThemeToggle.svelte
+++ src/components/navbar/ThemeToggle.svelte
@@ -22,13 +22,13 @@
 
 <button
   on:click={toggleDarkMode}
-  class="p-2 {y > 70
-    ? 'bg-[#393939]'
-    : 'bg-transparent'} rounded-lg hover:bg-yellow-500 hover:bg-opacity-20 duration-150 transition-all overflow-hidden h-[38px]"
+  class="p-2 hover:bg-yellow-500 {y > 70
+    ? 'bg-[#e1e1e1] dark:bg-[#393939] dark:hover:bg-yellow-500 dark:hover:bg-opacity-30'
+    : 'bg-transparent'} rounded-lg hover:bg-opacity-50 duration-150 transition-all overflow-hidden h-[38px]"
 >
-  {#if isDark}
+  {#if !isDark}
     <Moon strokeWidth={1.6} size={22} />
   {:else}
-    <Sun strokeWidth={1.6} size={22} color={"#ddd"} />
+    <Sun strokeWidth={1.6} size={22} />
   {/if}
 </button>
