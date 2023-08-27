3598961a368c05faa03b40b2446f5cf794935542
m4nute <mgudinocontacto@gmail.com>
2023-08-27 10:00:52 -0300

    Navbar

---
+++ src/components/navbar/NavButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import * as Tooltip from "$lib/shadcn/ui/tooltip"
+  export let text: string
+</script>
+
+<Tooltip.Root openDelay={120}>
+  <Tooltip.Trigger>
+    <button class="bg-[#393939] p-2 rounded-lg hover:scale-105 duration-150 transition-transform">
+      <slot />
+    </button>
+  </Tooltip.Trigger>
+  <Tooltip.Content class="bg-[#393939] font-bold border-gray-100 border-opacity-10">
+    <h4 class="text-xs">{text}</h4>
+  </Tooltip.Content>
+</Tooltip.Root>
---
+++ src/components/navbar/Navbar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import NavButton from './NavButton.svelte';
+	import { FileText, Mail, Github, Linkedin } from 'lucide-svelte';
+	import ThemeToggle from './ThemeToggle.svelte';
+	let y: number;
+</script>
+
+<nav
+	class="sticky top-2 rounded-xl bg-[#292929]/50 px-4 text-xl py-2 backdrop-blur-md flex justify-between"
+>
+	<section class="flex gap-3">
+		<NavButton text={'Linkedin'}><Linkedin strokeWidth={1.6} size={22} /></NavButton>
+		<NavButton text={'Github'}><Github strokeWidth={1.6} size={22} /></NavButton>
+		<NavButton text={'Mail'}><Mail strokeWidth={1.6} size={22} /></NavButton>
+		<NavButton text={'CV'}><FileText strokeWidth={1.6} size={22} /></NavButton>
+	</section>
+	<ThemeToggle />
+</nav>
+
+<svelte:window bind:scrollY={y} />
