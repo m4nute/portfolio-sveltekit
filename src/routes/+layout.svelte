03ec5041d46da5e7ede8e8efc9a2d1a606104bba
m4nute <mgudinocontacto@gmail.com>
2023-08-27 10:00:56 -0300

    Basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.postcss"
+  import Navbar from "../components/navbar/Navbar.svelte"
+</script>
+
+<main class="bg-white text-black dark:bg-[#1c1c1c] min-h-screen dark:text-white">
+  <div class="max-w-2xl mx-auto">
+    <Navbar />
+    <div class="px-3 pt-8">
+      <slot />
+    </div>
+  </div>
+</main>
