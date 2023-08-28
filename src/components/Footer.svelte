f3a5c99894962b95d6b09af729abba28577e9304
m4nute <mgudinocontacto@gmail.com>
2023-08-27 23:02:37 -0300

    Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,6 @@
+<section
+  class="border-t mx-3 border-gray-300 text-gray-700 dark:text-gray-300 px-1 text-sm py-2 dark:border-gray-600 border-opacity-50 transition-all flex justify-between"
+>
+  <p>Made with SvelteKit</p>
+  <p>@m4nute</p>
+</section>
