363c130a4809da2be1e7545f2d096b2e42c12ba8
m4nute <mgudinocontacto@gmail.com>
2023-08-27 23:03:12 -0300

    Added section to layout and homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script>
   import "../app.postcss"
+  import Footer from "../components/Footer.svelte"
   import Navbar from "../components/navbar/Navbar.svelte"
 </script>
 
 <main class="bg-white text-black dark:bg-[#1c1c1c] min-h-screen dark:text-white">
   <div class="max-w-2xl mx-auto">
     <Navbar />
-    <div class="px-4 pt-10">
+    <div class="px-4 pt-10 pb-6">
       <slot />
     </div>
+    <Footer />
   </div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script>
-  import Experience from "../components/homepage/Experience.svelte"
+  import Contact from "../components/homepage/Contact.svelte"
+  import Experience from "../components/homepage/experience/Experience.svelte"
   import Intro from "../components/homepage/Intro.svelte"
+  import Projects from "../components/homepage/projects/Projects.svelte"
+  import Stack from "../components/homepage/Stack.svelte"
 </script>
 
 <div>
   <Intro />
   <br /><br />
   <Experience />
+  <br /><br />
+  <Projects />
+  <br /><br />
+  <Stack />
+  <br /><br />
+  <Contact />
 </div>
