969c7d028e9de1b22e5fe81316ae60f0f0a863c6
m4nute <mgudinocontacto@gmail.com>
2023-08-27 23:02:42 -0300

    Contact section

---
+++ src/components/homepage/Contact.svelte
@@ -0,0 +1,5 @@
+<h5 class="underline underline-offset-4 mb-3">Get in Touch</h5>
+
+<p class="text-xs sm:text-sm tracking-widest text-gray-800 dark:text-gray-200">
+  Contact me through my email at <strong>mgud@proton.me</strong> or through my LinkedIn. I'm always open to discuss new opportunities. All links are above!
+</p>
