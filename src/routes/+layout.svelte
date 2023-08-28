4b29a0ab4131032782ac4303bcceed2bf9793b3f
m4nute <mgudinocontacto@gmail.com>
2023-08-28 12:49:38 -0300

    CV embedding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,20 @@
-<script>
+<script lang="ts">
   import "../app.postcss"
   import Footer from "../components/Footer.svelte"
   import Navbar from "../components/navbar/Navbar.svelte"
+  import { page } from "$app/stores"
 </script>
 
-<main class="bg-white text-black dark:bg-[#1c1c1c] min-h-screen dark:text-white">
-  <div class="max-w-2xl mx-auto">
-    <Navbar />
-    <div class="px-4 pt-10 pb-6">
-      <slot />
+{#if $page.url.pathname !== "/cv"}
+  <main class="bg-white text-black dark:bg-[#1c1c1c] min-h-screen dark:text-white">
+    <div class="max-w-2xl mx-auto">
+      <Navbar />
+      <div class="px-4 pt-10 pb-6">
+        <slot />
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-</main>
+  </main>
+{:else}
+  <slot />
+{/if}
