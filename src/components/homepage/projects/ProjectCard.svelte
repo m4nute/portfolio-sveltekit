a97cadcc3a0f5e9d253c566879593db206fb1312
m4nute <mgudinocontacto@gmail.com>
2023-08-27 23:02:33 -0300

    Project components

---
+++ src/components/homepage/projects/ProjectCard.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { Badge } from "$lib/shadcn/ui/badge"
+  import { Github } from "lucide-svelte"
+
+  export let project: string
+  export let date: string
+  export let badges: string[]
+  export let link: string
+  export let description: string
+  export let repo: string | null = null
+</script>
+
+<article class="flex flex-col sm:flex-row gap-2 sm:gap-6 mb-4">
+  <section class="flex flex-row sm:flex-col justify-between py-1 sm:py-4">
+    <div class="text-gray-800 dark:text-gray-200 text-sm w-24">{date}</div>
+    {#if repo}
+      <a
+        class="flex w-fit hover:text-black dark:hover:text-white transition-all text-sm text-gray-600 dark:text-gray-300 gap-1"
+        target="_blank"
+        href={repo}><Github size={22} strokeWidth={1.6} /> <span>Repo</span></a
+      >
+    {/if}
+  </section>
+  <a
+    target="_blank"
+    href={link}
+    class="flex-1 w-full hover:bg-[#eee] dark:hover:bg-[#112] cursor-pointer sm:p-4 rounded-lg transition-all duration-150"
+  >
+    <h6 class="font-bold text-lg">{project}</h6>
+    <p class="text-sm text-gray-700 dark:text-gray-300 mt-1 mb-3">{description}</p>
+    <ol class="gap-2 flex mt-3 flex-wrap">
+      {#each badges as badge}
+        <Badge>{badge}</Badge>
+      {/each}
+    </ol>
+  </a>
+</article>
---
+++ src/components/homepage/projects/Projects.svelte
@@ -0,0 +1,39 @@
+<script>
+  import ProjectCard from "./ProjectCard.svelte"
+</script>
+
+<h5 class="underline underline-offset-4 mb-3">Projects</h5>
+<ol>
+  <li class="flex flex-col gap-y-4">
+    <ProjectCard
+      project="This Portfolio"
+      description="Personal Portfolio where I showcase my work, projects and skills. Also learned Svelte and testing along the way!"
+      date="Aug 2023"
+      link="https://manuelgudino.vercel.app/"
+      badges={["SvelteKit", "Tailwind CSS", "Playwright", "Vitest"]}
+      repo="https://github.com/m4nute/portfolio-sveltekit"
+    />
+    <ProjectCard
+      project="Personal Blog"
+      description="Personal blog where I talk about the technologies I'm passionate about and share my experiences and knowledge about them."
+      date="May 2023"
+      link="https://manuelgudino-blog.vercel.app"
+      badges={["Next.js", "Tailwind CSS", "ContentLayer"]}
+      repo="https://github.com/m4nute/blog-next13-contentlayer"
+    />
+    <ProjectCard
+      project="Realtime Chat App"
+      description="Contains Google and credentials auth, private chat rooms, realtime messages and chat list ordered by last update."
+      date="May 2023"
+      link="https://m4nute-chat.vercel.app/"
+      badges={["Next.js", "Tailwind CSS", "Supabase", "Zustand", "React Query"]}
+      repo="https://github.com/m4nute/next-supa-chat"
+    />
+  </li>
+</ol>
+<br />
+<div class="text-center">
+  <a href="/projects" class="underline underline-offset-2 text-gray-700 hover:text-black dark:text-gray-300 dark:hover:text-white transition-all"
+    >View All Projects</a
+  >
+</div>
